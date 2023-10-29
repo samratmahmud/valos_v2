17bb14fbfe964ea5c1e66070b603b175d2e097a8
samratmahmud <samratmahmud1999@gmail.com>
2023-10-30 00:12:36 +0600

    valos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/global/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-cyan-950">
       <Navbar />
       {/* <Header /> */}
     </div>
--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -2,8 +2,28 @@ import React from "react";
 
 function Navbar() {
   return (
-    <section className="container">
-      <div className="text-4xl">Samrat</div>
+    <section>
+      <div className="flex gap-[60px] items-center justify-between">
+        <div
+          className="bg-no-repeat bg-cover min-w-[570px] flex gap-36 pt-5 pb-4"
+          style={{backgroundImage: `url('/picture/Vector.png')`}}
+        >
+          <div className="ml-[138px] text-xl font-semibold text-white">
+            Services
+          </div>
+          <div className=" text-xl font-semibold text-white">Technology</div>
+        </div>
+        <div className="mt-8">
+          <img src="/picture/Vector.svg" alt="" />
+        </div>
+        <div
+          className="bg-no-repeat bg-cover min-w-[590px] flex gap-36 pt-5 pb-4 justify-end"
+          style={{backgroundImage: `url('/picture/Vector (1).png')`}}
+        >
+          <div className="text-xl font-semibold text-white">Request</div>
+          <div className="mr-36 text-xl font-semibold text-white">Contact</div>
+        </div>
+      </div>
     </section>
   );
 }
